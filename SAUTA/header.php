2c040e8b2816d2c5a8de66daf0260ef6523ce1f1
Jair Ferreira Neto Neves <jair.neto@valec.gov.br>
2018-05-09 12:17:57 -0300

    separação de header, footer e index

---
+++ SAUTA/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head lang="pt-BR">
+	
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<title>SAUTA</title>	
+		
+	</head>
+	
+	<body>
+		<header>
+		</header>
