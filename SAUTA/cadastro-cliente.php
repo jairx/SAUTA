38c92008ff30846362c3e190a08fd65c63378a6a
jairx <jairblz@gmail.com>
2018-06-04 18:00:56 -0300

    telas do sistema

---
+++ SAUTA/cadastro-cliente.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+	<html>
+	<head lang="pt-BR">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>SAUTA</title>	
+	</head>
+	<body>
+		</header>
+            
+		</header>
+
+        <h1>Cadastro de Cliente</h1>
+
+		<form action="home.php">
+			
+            Nome:
+			<input type="text" name="nome"></br>
+			CPF:
+            <input type="number" name="cpf"></br>
+            Data de Nascimento:
+            <input type="date" name="data_nascimento"></br>
+            E-mail:
+            <input type="text" name="email"></br>
+            Telefone:
+            <input type="number" name="tel"></br>
+            Celular:
+            <input type="number" name="cel"></br>
+			<input type="submit" value="Cadastrar">
+		
+		</form>
+
+		<footer>
+		</footer>
+
+	</body>
+</html>
---
+++ SAUTA/cadastro-reserva.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+	<html>
+	<head lang="pt-BR">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>SAUTA</title>	
+	</head>
+	<body>
+		</header>
+            
+		</header>
+
+        <h1>Cadastro de Cliente</h1>
+
+		<form action="home.php">
+			
+            Data de Nascimento:
+            <input type="date" name="data_nascimento"></br>
+            Horário: 
+            <input type="time" name="horario"></br>
+            Número de pessoas:
+            <input type="number" name="numero_pessoas"></br>
+            Mesa:
+            <input type="number" name="mesa"></br>
+		
+		</form>
+
+		<footer>
+		</footer>
+
+	</body>
+</html>
