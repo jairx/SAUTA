92f3a5358f2ecf6e7d62e46e432b551223c1ea94
jairx <jairblz@gmail.com>
2018-06-04 16:34:38 -0300

    junção para facilitar

--- SAUTA/index.php
+++ SAUTA/index.php
@@ -1,8 +1,24 @@
 <!DOCTYPE HTML>
-<html>
+	<html>
 	<head lang="pt-BR">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>SAUTA</title>	
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>SAUTA</title>	
 	</head>
 	<body>
+		</header>
+		</header>
+
+		<form action="home.php">
+			Nome:
+			<input type="text" name="nome"></br>
+			
+			<input type="submit" value="Cadastrar">
+		
+		</form>
+
+		<footer>
+		</footer>
+
+	</body>
+</html>
