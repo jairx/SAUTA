4d891bdbdce7d951a6f0c1e33c6c7788dcd8dad1
jairx <jairblz@gmail.com>
2018-06-08 17:39:32 -0300

    quase nada

--- SAUTA/index.php
+++ SAUTA/index.php
@@ -4,13 +4,14 @@
 		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 			<div class="navbar-nav">
 				<a class="nav-item nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
-				<a class="nav-item nav-link" href="formulario-cliente.php">Cadastro</a>
 			</div>
 		</div>
 	</nav>
 
 	<h1>Tela de Clientes</h1></br></br></br></br>
 
-	<h2><a href="">Pedidos</a></h2></br></br>
+	<h2><a href="formulario-pedido.php">Pedidos</a></h2></br></br>
+
+	<h2><a href="formulario-reserva.php">Reservas</a></h2></br></br>
 
 <?php include("footer.php"); ?>
