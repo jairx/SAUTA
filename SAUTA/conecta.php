af0790e17fd516e666d19c476bca04e270a3fee7
Jair Ferreira Neto Neves <jair.neto@valec.gov.br>
2018-06-13 12:16:27 -0300

    teste

--- SAUTA/conecta.php
+++ SAUTA/conecta.php
@@ -1,3 +1,3 @@
 <?php
 
-    $conexao = mysqli_connect('localhost', 'u968512456_teamx', 'Copa2018', 'u968512456_sauta');
+    $conexao = mysqli_connect('localhost', 'u968512456_teamx', 'Copa2018', 'u968512456_sauta');//
